570cdc61a1716f6c20ba580f7637e99ccc330f05
zfx1101804091 <1875586562@qq.com>
2020-03-21 22:52:39 +0800

    注释私密

--- src/main/java/com/aliyun/sms/model/AliSmsConfig.java
+++ src/main/java/com/aliyun/sms/model/AliSmsConfig.java
@@ -1,7 +1,7 @@
 package com.aliyun.sms.model;
 
 public class AliSmsConfig {
-    public static final String SignName = "华丽服饰特卖";
-    public static final String TemplateCode = "SMS_186575948";
+    public static final String SignName = "*****";
+    public static final String TemplateCode = "*******";
     public static final long timeOut = 1;//短信验证码有效期
 }
