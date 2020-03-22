a628b264362d2f2246f7004b12f37dee3b7290c6
zfx1101804091 <1875586562@qq.com>
2020-03-22 17:34:46 +0800

    20200233 17点34分

--- src/main/java/com/aliyun/sms/AliConfigApp.java
+++ src/main/java/com/aliyun/sms/AliConfigApp.java
@@ -2,8 +2,6 @@ package com.aliyun.sms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * @description:
